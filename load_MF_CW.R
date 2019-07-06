c3e58b53bfeac5efa2fc4343161fa06cd0bb01d3
Jim Lutz <jdlutz@hotwaterresearch.net>
2019-07-05 18:40:48 -0700

    turns out RASS doesn't ask about loads of laundry if you don't have laundry equipment in your home

--- load_MF_CW.R
+++ load_MF_CW.R
@@ -1,6 +1,6 @@
 # load_MF_CW.R
 # script to load MF CW data from ./data/DT_RASS.Rdata
-# and make plots
+# and look at data
 # started by Jim Lutz "Fri Jul  5 16:54:07 2019"
 
 # set packages & etc
@@ -53,7 +53,65 @@ DT_RASS[NR0_5!=97, list(n=sum(wt)),by=NR0_5][order(NR0_5)]
 ggplot(data = DT_RASS[NR0_5!=97], aes(x=NR0_5, y=wt) ) +
   geom_col()
 
+# look at DWLTYPE to see if it's multifamily
+DT_RASS[ , list(n=length(wt)),by=DWLTYPE][order(DWLTYPE)]
+  #    DWLTYPE     n
+  # 1:       1 14263  Single-family detached house
+  # 2:       2  1828  Townhouse, duplex, or row house
+  # 3:       3  1900  Apartment or condominium (2 – 4 units)
+  # 4:       4  4062  Apartment or condominium (5 or more units)
+  # 5:       5  1703  Mobile home
+  # 6:       6   300  Other
+  # 7:      97  1665  NA?
 
+# Do you have the use of laundry equipment in your home? (LNDRYEQP)
+DT_RASS[ , list(n=length(wt)), by=LNDRYEQP]
+#    LNDRYEQP     n 
+# 1:        1 21291 Yes
+# 2:        3  1046 No, laundry facilities are located in a common area of the building.
+# 3:        2  3384 I do not use laundry facilities in my building 
 
+# see about LNDRYEQP vs CWHWLD CWWWLD CWCWLD
+DT_RASS[ , list(n=length(wt)), by=CWHWLD]
+
+# tally clothes washer loads per week 
+# but not 99 or 97
+DT_RASS[CWHWLD==97 | CWHWLD==99, CWHWLD:=0]
+DT_RASS[CWWWLD==97 | CWWWLD==99, CWWWLD:=0]
+DT_RASS[CWCWLD==97 | CWCWLD==99, CWCWLD:=0]
+DT_RASS[, CWLD:= CWHWLD + CWWWLD + CWCWLD ]
+
+DT_RASS[ , list(n=length(wt)), by=CWLD][order(CWLD)]
+
+# compare to DWLTYPE
+DT_RASS[ LNDRYEQP==1 , list(ave_loads=mean(CWLD)),by=DWLTYPE]
+#    DWLTYPE ave_loads
+# 1:       2  4.235257
+# 2:       1  4.856844
+# 3:       5  3.980641
+# 4:       4  3.667508
+# 5:       3  3.891816
+# 6:       6  4.242991
+# 7:      97  4.557796
+
+DT_RASS[ LNDRYEQP==2 , list(ave_loads=mean(CWLD)),by=DWLTYPE]
+#    DWLTYPE ave_loads
+# 1:       1         0
+# 2:       4         0
+# 3:       3         0
+# 4:       2         0
+# 5:      97         0
+# 6:       5         0
+# 7:       6         0
+
+DT_RASS[ LNDRYEQP==3 , list(ave_loads=mean(CWLD)),by=DWLTYPE]
+DWLTYPE ave_loads
+# 1:       1         0
+# 2:       4         0
+# 3:       2         0
+# 4:       3         0
+# 5:       6         0
+# 6:      97         0
+# 7:       5         0
 
 
